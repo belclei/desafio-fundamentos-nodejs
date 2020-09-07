b32ce2b6a2afb5ff7d9e06601cdfed11f969f0a8
belclei <belclei@gmail.com>
2020-09-07 00:24:58 -0300

    Challenge Completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,11 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +19,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulatedBalance: Balance, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            return {
+              income: accumulatedBalance.income + transaction.value,
+              outcome: accumulatedBalance.outcome,
+              total: 0,
+            };
+          case 'outcome':
+            return {
+              income: accumulatedBalance.income,
+              outcome: accumulatedBalance.outcome + transaction.value,
+              total: 0,
+            };
+          default:
+            break;
+        }
+        return accumulatedBalance;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(createTransactionDTO: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction(createTransactionDTO);
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
